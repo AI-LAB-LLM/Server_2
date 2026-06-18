e8aebdad126f26f81bcbfa16a62e1b6e92df18c8
Zuyeonn <joannaparo0417@gmail.com>
2026-06-18 14:23:41 +0900

    feat: admin 추가

--- geo/admin.py
+++ geo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import GeoData
+from .models import GeoData, GeoProcessedData, GeoTripAnomalyResult
 
 
 @admin.register(GeoData)
@@ -17,3 +17,50 @@ class GeoDataAdmin(admin.ModelAdmin):
     list_filter = ("pos_success",)
     search_fields = ("device_id", "protectee__name", "protectee__device_id")
     ordering = ("-timestamp",)
+
+
+@admin.register(GeoProcessedData)
+class GeoProcessedDataAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "device_id",
+        "timestamp",
+        "pos_success",
+        "raw_latitude",
+        "raw_longitude",
+        "latitude",
+        "longitude",
+        "gps_quality",
+        "gps_filter_decision",
+        "state_primary",
+    )
+    list_filter = ("pos_success", "gps_quality", "gps_filter_decision", "state_primary")
+    search_fields = ("device_id", "protectee__name", "protectee__device_id")
+    ordering = ("-timestamp",)
+    readonly_fields = (
+        "predicted_latitude",
+        "predicted_longitude",
+        "predicted_uncertainty_m",
+        "predicted_confidence_level",
+        "created_at",
+        "updated_at",
+    )
+
+
+@admin.register(GeoTripAnomalyResult)
+class GeoTripAnomalyResultAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "device_id",
+        "trip_start_time",
+        "trip_end_time",
+        "final_route_label",
+        "od_key",
+        "dtw_score",
+        "threshold",
+        "created_at",
+    )
+    list_filter = ("final_route_label",)
+    search_fields = ("device_id", "protectee__name", "protectee__device_id", "od_key")
+    ordering = ("-trip_start_time",)
+    readonly_fields = ("created_at",)
