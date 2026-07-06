f52af2164b8c9b76b0b6eb9c08e0cf810f66d548
Zuyeonn <joannapark0417@gmail.com>
2026-07-06 21:38:11 +0900

    feat: geo 시각화 url 추가

--- geo/urls.py
+++ geo/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import GeoDataIngestView, GeoTrackDataView
+from .views import GeoDataIngestView, GeoTrackDataView, GeoTripMapView, GeoTripHistoryView, GeoTripCalendarView
 
 urlpatterns = [
     path("data", GeoDataIngestView.as_view(), name="geo-data"),
     path("track", GeoTrackDataView.as_view(), name="geo-track"),
+    path("trip_map/", GeoTripMapView.as_view(), name="geo-trip-map"),
+    path("trip_history/", GeoTripHistoryView.as_view(), name="geo-trip-history"),
+    path("trip_calendar/", GeoTripCalendarView.as_view(), name="geo-trip-calendar"),
 ]
